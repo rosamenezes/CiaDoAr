d94f082f01c1e6e8be96bfb56c4c5f658d1cdb06
Guilherme Menezes <guilhermemenezes@MacBookAir.station>
2024-11-21 20:54:47 +0100

    Style Home Screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { useRouter } from 'expo-router';
 import { View, Text, Image, Pressable } from 'react-native';
-import logo from '../assets/images/logo.jpeg';
+import logo from '../assets/images/logoSimple.png';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
-  const buttonStyle = "bg-pink-600 py-3 px-10 rounded-full w-full max-w-xs mx-auto mb-4";
+  const primaryButtonStyle = "bg-pink-600 py-4 px-12 rounded-full w-11/12 max-w-xs mx-auto mb-6 shadow-sm";
+  const secondaryButtonStyle = "bg-white border-2 border-pink-600 py-4 px-12 rounded-full w-11/12 max-w-xs mx-auto mb-6 shadow-sm";
   const router = useRouter();
 
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Image
-        source={logo}
-        className="w-80 h-80 mb-12"
-        resizeMode="contain"
-      />
-      <Pressable onPress={() => router.push('/criar')} className={buttonStyle} accessibilityLabel="Botão para criar novo item">
-        <Text className="text-white text-center font-bold">Criar</Text>
+      <View className="flex items-center mb-16">
+        <Text className="text-4xl font-bold text-gray-800 mb-2">FlyCheck</Text>
+        <Text className="text-1xl text-gray-500">Gerenciado de Laudos de Parapente</Text>
+        <Image
+          source={logo}
+          style={{ width: 160, height: 160, marginBottom: 24, }}
+          resizeMode="contain"
+        />
+      </View>
+      <Pressable onPress={() => router.push('/criar')} className={primaryButtonStyle} accessibilityLabel="Botão para criar novo item">
+        <Text className="text-white text-center font-bold text-lg">Criar</Text>
       </Pressable>
-      <Pressable onPress={() => router.push('/salvos')} className={buttonStyle} accessibilityLabel="Botão para acessar itens salvos">
-        <Text className="text-white text-center font-bold">Salvos</Text>
+      <Pressable onPress={() => router.push('/salvos')} className={secondaryButtonStyle} accessibilityLabel="Botão para acessar itens salvos">
+        <Text className="text-pink-600 text-center font-bold text-lg">Salvos</Text>
       </Pressable>
     </View>
   );
